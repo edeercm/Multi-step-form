131af454aaf675043a92f5e88996cbb77b76a365
Eder <132967872+edeercm@users.noreply.github.com>
2024-03-27 18:30:58 -0600

    Update

--- src/components/BgImage.jsx
+++ src/components/BgImage.jsx
@@ -4,15 +4,15 @@ import bgdesk from '../assets/images/bg-desk.svg';
 import bgmobile from '../assets/images/bg-mobile.svg';
 
 const BgImg = styled.img`
-  content: url(${bgdesk});
+  /* content: url(${bgdesk});
 
   @media (max-width: 991.98px) {
     content: url(${bgmobile});
-  }
+  } */
 `;
 
 const BgImage = () => {
-  return <BgImg alt="Background" />;
+  return <img src={window.innerWidth <= 991.98 ? bgmobile : bgdesk } alt="Background" />;
 }
 
 export default BgImage
